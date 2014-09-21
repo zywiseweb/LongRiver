35caa50452d6400f50595a0caef1cf8a93ea92d3
Jia Chengbin <joo00oot@gmail.com>
2014-09-21 18:18:12 +0800

    完善了部门控制以及显示。

--- system_modules/department/department.js
+++ system_modules/department/department.js
@@ -21,13 +21,48 @@ var departmentManager = mongoose.model('Department', departmentSchema);
  * @param {type} callback
  * @returns {undefined}
  */
-exports.findDepartment = function(where, callback) {
-    departmentManager.findOne(where, callback);
+exports.findDepartment = function (id, callback) {
+    departmentManager.findOne({id: '100'}, function (err, root) {
+        if (err) {
+            callback(err);
+        } else {
+          /* console.info(id);
+            console.info(root);
+            var d =getDepartmentByID(id,root);
+            if(d){
+               callback(null, {id:'0',text:'没有部门'});   
+            }else{
+              callback(null, d);   
+            }*/
+            
+             callback(null, root); 
+        }
+    });
 };
+/**
+ * 通过id找到部门
+ * @param {type} id
+ * @param {type} parentDap
+ * @returns {unresolved}
+ */
+function getDepartmentByID(id, parentDap) {
+    // console.info(id +" ===="+ parentDap.id);
+    if (id === parentDap.id) {
+        return parentDap;
+    } else {
+        if (parentDap.children && parentDap.children.length > 0) {
+            for (var i = 0; i < parentDap.children.length; i++) {
+                return  getDepartmentByID(id, parentDap.children[i]);
+            }
+        }
+    }
 
-exports.saveDepartment = function(department, callback) {
+
+}
+
+exports.saveDepartment = function (department, callback) {
     var newdepartment = new departmentManager(department);
-    newdepartment.save(function(err) {
+    newdepartment.save(function (err) {
         if (err) {
             callback(err, '保存部门数据错误');
             console.log(err);
@@ -39,9 +74,9 @@ exports.saveDepartment = function(department, callback) {
 
 };
 
-exports.updateDepartment = function(department, callback) {
+exports.updateDepartment = function (department, callback) {
     var root = getdepartment(department[0]);
-    departmentManager.update({id: '100'}, {$set: {children: root.children}}, function(err) {
+    departmentManager.update({id: '100'}, {$set: {children: root.children}}, function (err) {
         if (err) {
             callback(err, '保存部门数据错误');
         } else {
@@ -56,6 +91,7 @@ function getdepartment(object) {
     var d = {
         id: object.id,
         text: object.text,
+        info: object.info,
         children: []
     };
     if (object.children) {
@@ -75,39 +111,40 @@ function getdepartment(object) {
  * @param {type} callback
  * @returns {undefined}
  */
-exports.getDepartmentList = function(id, callback) {
-    departmentManager.find({id: 100}, function(err, result) {
-        if (err) {
-            callback(err);
-        } else {
-            var list = [];
-            var base = result[0];
-            for (var i = 0; i < base.children.length; i++) {
-                var d = {
-                    id: base.children[i].id,
-                    text: base.children[i].text
-                };
-                list.push(d);
-                getChildren(base.children[i], list);
-            }
-            callback(null, list);
-        }
-    });
-};
-
-function getChildren(dp, list) {
-    
-    if (dp.children && dp.children.length > 0) {
-        for (var i = 0; i < dp.children.length; i++) {
-            var d = {
-                id: dp.children[i].id,
-                text: dp.children[i].text
-            };
-            list.push(d);
-            getChildren(dp.children[i], list);
-        }
-    }
-}
-
-
-
+/*
+ exports.getDepartmentList = function(id, callback) {
+ departmentManager.find({id: 100}, function(err, result) {
+ if (err) {
+ callback(err);
+ } else {
+ var list = [];
+ var base = result[0];
+ for (var i = 0; i < base.children.length; i++) {
+ var d = {
+ id: base.children[i].id,
+ text: base.children[i].text
+ };
+ list.push(d);
+ getChildren(base.children[i], list);
+ }
+ callback(null, list);
+ }
+ });
+ };
+ 
+ function getChildren(dp, list) {
+ 
+ if (dp.children && dp.children.length > 0) {
+ for (var i = 0; i < dp.children.length; i++) {
+ var d = {
+ id: dp.children[i].id,
+ text: dp.children[i].text
+ };
+ list.push(d);
+ getChildren(dp.children[i], list);
+ }
+ }
+ }
+ 
+ 
+ */
